926721948b19e911ab393d75580f78afe791f9e2
Arc1llusion <arc1llusion@gmail.com>
2022-03-03 22:53:45 -0500

    Make .net API respect dto casing

--- CoinCounting-Api2/Program.cs
+++ CoinCounting-Api2/Program.cs
@@ -5,8 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers()
+    .AddJsonOptions(opts => opts.JsonSerializerOptions.PropertyNamingPolicy = null);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
